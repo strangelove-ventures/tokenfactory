e6f6f0658fb65ab8e82356c276b4464e31236b9f
Reece Williams <reecepbcups@gmail.com>
2024-05-19 14:55:25 -0500

    further base test

--- x/tokenfactory/ante/before_send_hook.go
+++ x/tokenfactory/ante/before_send_hook.go
@@ -3,6 +3,7 @@ package decorators
 import (
 	"fmt"
 
+	authz "github.com/cosmos/cosmos-sdk/x/authz"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/strangelove-ventures/tokenfactory/x/tokenfactory/keeper"
 	tokenfactorytypes "github.com/strangelove-ventures/tokenfactory/x/tokenfactory/types"
@@ -21,37 +22,54 @@ func NewMsgBeforeSendHook(k keeper.Keeper) MsgBeforeSendHook {
 }
 
 func (mfd MsgBeforeSendHook) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	mfd.applyDenomHookIfApplicable(tx.GetMsgs())
+	if err := mfd.applyDenomHookIfApplicable(tx.GetMsgs()); err != nil {
+		return ctx, err
+	}
+
 	return next(ctx, tx, simulate)
 }
 
-func (mfd MsgBeforeSendHook) applyDenomHookIfApplicable(msgs []sdk.Msg) {
+func (mfd MsgBeforeSendHook) applyDenomHookIfApplicable(msgs []sdk.Msg) error {
 	for _, msg := range msgs {
-		// TODO: authz nested check here
+		// Check if an authz message, loop through all inner messages, and recursively call this function
+		if execMsg, ok := msg.(*authz.MsgExec); ok {
+			msgs, err := execMsg.GetMessages()
+			if err != nil {
+				return err
+			}
 
+			// Recursively call this function with the inner messages
+			if err = mfd.applyDenomHookIfApplicable(msgs); err != nil {
+				return err
+			}
+		}
+
+		// if it is a bank message, perform the action if it's a tokenfactory denom
 		if m, ok := msg.(*banktypes.MsgSend); ok {
-			mfd.performAction(m)
+			// mfd.performAction(m)
+
+			sender := m.FromAddress
+			recipient := m.ToAddress
+			coins := m.Amount
+
+			for _, coin := range coins {
+				coin := coin
+
+				// validate is a tokenfactory denom
+				_, _, err := tokenfactorytypes.DeconstructDenom(coin.Denom)
+				if err != nil {
+					continue
+				}
+
+				// TODO: see if hooks is registered
+
+				// This is a validate tokenfactory denom being sent, check if it is registered for hooks
+				fireEvent(sender, recipient, coin)
+			}
 		}
 	}
-}
 
-func (mfd MsgBeforeSendHook) performAction(msg *banktypes.MsgSend) {
-	sender := msg.FromAddress
-	recipient := msg.ToAddress
-	coins := msg.Amount
-
-	for _, coin := range coins {
-		coin := coin
-
-		// validate is a tokenfactory denom
-		_, _, err := tokenfactorytypes.DeconstructDenom(coin.Denom)
-		if err != nil {
-			continue
-		}
-
-		// This is a validate tokenfactory denom being sent, check if it is registered for hooks
-		fireEvent(sender, recipient, coin)
-	}
+	return nil
 }
 
 func fireEvent(sender, recipient string, coin sdk.Coin) error {
--- x/tokenfactory/ante/before_send_test.go
+++ x/tokenfactory/ante/before_send_test.go
@@ -2,10 +2,12 @@ package decorators_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	sdkmath "cosmossdk.io/math"
 
+	"github.com/CosmWasm/wasmd/x/wasm/keeper"
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -22,36 +24,73 @@ var (
 )
 
 func TestBeforeSend(t *testing.T) {
-	// generate a private/public key pair and get the respective address
-	pk1 := ed25519.GenPrivKey().PubKey()
-	fromAddr := sdk.AccAddress(pk1.Address())
+	fromAddr := createAccount()
+	toAddr := createAccount()
 
-	pk2 := ed25519.GenPrivKey().PubKey()
-	toAddr := sdk.AccAddress(pk2.Address())
-
-	// app, ctx := bindings.SetupCustomApp(t, addr1)
+	// Setup chain and single ante
 	ctx, chain := app.Setup(t)
-
-	token, err := chain.TokenFactoryKeeper.CreateDenom(ctx, fromAddr.String(), "bitcoin")
-	require.NoError(t, err)
-
-	fmt.Println(token)
-
-	// Create change rate decorator
 	anteHandler := ante.NewMsgBeforeSendHook(chain.TokenFactoryKeeper)
 
+	// Create new token
+	token, err := chain.TokenFactoryKeeper.CreateDenom(ctx, fromAddr.String(), "bitcoin")
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	fmt.Println("token", token)
+
+	// Mint new coins to the fromAddr
+	initCoins := newCoins(token, 100_000)
+	err = chain.BankKeeper.MintCoins(ctx, "mint", initCoins)
+	require.NoError(t, err)
+	err = chain.BankKeeper.SendCoinsFromModuleToAccount(ctx, "mint", fromAddr, initCoins)
+	require.NoError(t, err)
+
+	// Setup the hooks contract
+	// TODO: change me (and the readme) to proper contract sudo bindings
+	codeID := storeHooksContract(t, ctx, chain, fromAddr)
+	cAddr := instantiateBeforeSendContract(t, ctx, chain, fromAddr, codeID)
+	fmt.Println(cAddr.String())
+
+	// TODO: register contract -> token (match osmosis spec)
+
+	// TODO: validate execution
 	bankSendMsg := banktypes.MsgSend{
 		FromAddress: fromAddr.String(),
 		ToAddress:   toAddr.String(),
-		Amount:      sdk.NewCoins(sdk.NewCoin(token, sdkmath.NewInt(100))),
+		Amount:      newCoins(token, 117),
 	}
 
 	if _, err := anteHandler.AnteHandle(ctx, NewMockTx(&bankSendMsg), false, EmptyAnte); err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	// change this to a hooks contract (on call increase, ref clock)
-	// reflect := instantiateReflectContract(t, ctx, app, actor)
-	// require.NotEmpty(t, reflect)
+}
 
+func storeHooksContract(t *testing.T, ctx sdk.Context, app *app.TokenFactoryApp, addr sdk.AccAddress) uint64 {
+	wasmCode, err := os.ReadFile("../bindings/testdata/hooks.wasm")
+	require.NoError(t, err)
+
+	contractKeeper := keeper.NewDefaultPermissionKeeper(app.WasmKeeper)
+	codeID, _, err := contractKeeper.Create(ctx, addr, wasmCode, nil)
+	require.NoError(t, err)
+
+	return codeID
+}
+
+func instantiateBeforeSendContract(t *testing.T, ctx sdk.Context, app *app.TokenFactoryApp, funder sdk.AccAddress, codeID uint64) sdk.AccAddress {
+	initMsgBz := []byte("{}")
+	contractKeeper := keeper.NewDefaultPermissionKeeper(app.WasmKeeper)
+
+	addr, _, err := contractKeeper.Instantiate(ctx, codeID, funder, funder, initMsgBz, "hooks contract", nil)
+	require.NoError(t, err)
+
+	return addr
+}
+
+func createAccount() sdk.AccAddress {
+	pk1 := ed25519.GenPrivKey().PubKey()
+	return sdk.AccAddress(pk1.Address())
+}
+
+func newCoins(token string, amt int64) sdk.Coins {
+	return sdk.NewCoins(sdk.NewCoin(token, sdkmath.NewInt(amt)))
 }
