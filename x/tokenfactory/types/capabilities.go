28c1188d63931331c9c65d247c7bcd7d92098912
Reece Williams <reecepbcups@gmail.com>
2024-12-16 09:37:48 -0600

    M-02 add clarrification note

--- x/tokenfactory/types/capabilities.go
+++ x/tokenfactory/types/capabilities.go
@@ -5,7 +5,10 @@ const (
 	EnableForceTransfer = "enable_force_transfer"
 	EnableBurnFrom      = "enable_burn_from"
 	// Allows addresses of your choosing to mint tokens based on specific conditions.
-	// via the IsSudoAdminFunc
+	// via the IsSudoAdminFunc.
+	// NOTE: with SudoMint enabled, the sudo admin can mint `any` token, not just tokenfactory tokens.
+	// This is intended behavior as requested by other teams, rather than having its own module with very minor logic.
+	// If you do not wish for this behavior, write your own and do not use this capability.
 	EnableSudoMint = "enable_admin_sudo_mint"
 	// EnableCommunityPoolFeeFunding sends tokens to the community pool when a new fee is charged (if one is set in params).
 	// This is useful for ICS chains, or networks who wish to just have the fee tokens burned (not gas fees, just the extra on top).
